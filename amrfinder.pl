5fbf85a99a9d0662c410734ab6d5a65396df4998
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2018-12-18 13:46:08 -0500

    Added Public Domain statement to amrfinder.pl source code

--- amrfinder.pl
+++ amrfinder.pl
@@ -1,4 +1,24 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl 
+#
+# PUBLIC DOMAIN NOTICE This software/database is "United States Government
+# Work" under the terms of the United States Copyright Act. It was written as
+# part of the authors' official duties for the United States Government and
+# thus cannot be copyrighted. This software/database is freely available to the
+# public for use without a copyright notice. Restrictions cannot be placed on
+# its present or future use.
+# 
+# Although all reasonable efforts have been taken to ensure the accuracy and
+# reliability of the software and data, the National Center for Biotechnology
+# Information (NCBI) and the U.S. Government do not and cannot warrant the
+# performance or results that may be obtained by using this software or data.
+# NCBI, NLM, and the U.S. Government disclaim all warranties as to performance,
+# merchantability or fitness for any particular purpose.
+# 
+# In any work or product derived from this material, proper attribution of the
+# authors as the source of the software or data should be made, using:
+# https://ncbi.nlm.nih.gov/pathogens/antimicrobial-resistance/AMRFinder/ as the
+# citation.
+# 
 use strict;
 use warnings;
 #use diagnostics;
@@ -7,7 +27,7 @@ use File::Temp qw( tempdir );
 use File::Basename;
 use Net::FTP;
 use Cwd qw( getcwd abs_path );
-my $curr_version = '$Revision: 38801 $';
+my $curr_version = '$Revision: 38913 $';
 our $DEBUG = 0;
 
 # todo:
