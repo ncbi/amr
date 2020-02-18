d1ac3bc3b1e4626442339ede689a8bbb7085cd98
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-02-18 17:05:25 -0500

    Adding test for binary installation

---
+++ .github/workflows/get_binary_release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# found and lightly modified these functions
+get_latest_release() {
+    curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    cut -d '"' -f 4
+}
+
+get_tarball_url() {
+    curl --silent "https://api.github.com/repos/$1/releases/latest" |
+        fgrep '"browser_download_url":' |
+        cut -d '"' -f 4
+}
+
+
+release=$(get_latest_release ncbi/amr)
+URL=$(get_tarball_url ncbi/amr)
+
+mkdir amrfinder
+cd amrfinder
+
+# download and unpack AMRFinder binaries
+    curl --silent -L -O $URL
+    tarball_name=$(echo $URL | perl -pe 's#^.*/(.*)#\1#')
+    tar xfz $tarball_name
+    rm $tarball_name
+
+# download and unpack test
+    curl --silent \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_dna.fa \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.fa \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.gff \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_both.expected \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_dna.expected \
+         -O https://raw.githubusercontent.com/ncbi/amr/master/test_prot.expected
+
+# download database
+    ./amrfinder --update
