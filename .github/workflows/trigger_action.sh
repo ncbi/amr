adde6423d16a564a65eed56a806eae6d7171be54
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2020-04-07 13:01:20 -0400

    Added script to manually trigger actons
    To make it easier to manually trigger actions I added a script
    to do so.

---
+++ .github/workflows/trigger_action.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo "Actions that can be triggered:"
+cat <<END
+1. install-test
+2. linux-compile-test
+3. linux-binary-test
+4. linux-bioconda-test
+5. mac-bioconda-test
+END
+
+echo -n "% "
+read action
+
+if [ "X$action" == "X" ]
+then
+    echo "Enter an action to take next time you run"
+    exit 1;
+fi
+
+echo $action
+case $action in 
+    1|install-test) event=install-test;;
+    2|linux-compile-test) event=linux-compile-test;;
+    3|linux-binary-test) event=linux-binary-test;;
+    4|linux-bioconda-test) event=linux-bioconda-test;;
+    5|mac-bioconda-test) event=mac-bioconda-test;;
+    *) echo "Unknown action. Please try again"
+        exit 1;;
+esac
+
+echo "action=$action event=$event"
+
+cmd="
+curl \
+  -H 'Accept: application/vnd.github.everest-preview+json' \
+  -H 'Authorization: token `cat ~/.github-token`' \
+  --data '{\"event_type\": \"$event\"}' \
+  --request POST \
+  https://api.github.com/repos/evolarjun/amr/dispatches
+"
+echo $cmd
+echo $cmd | bash
+
