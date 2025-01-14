8c72057d0fa26475900a3a93d63393420694e7d7
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2025-01-14 15:22:44 -0500

    StxTyper 1.0.31 - restores closest reference accesion

--- amrfinder.cpp
+++ amrfinder.cpp
@@ -33,6 +33,7 @@
 * Dependencies: NCBI BLAST, HMMer, libcurl, gunzip (optional)
 *
 * Release changes:
+*   4.0.15  01/14/2025 PD-5215  StxTu[er ver. 1.0.31 of branch "gpipe_compat"
 *   4.0.14  01/14/2025 PD-5215  StxTyper ver. 1.0.30 of branch "gpipe_compat"
 *   4.0.13  01/10/2025 PD-5215  StxTyper ver. 1.0.29 of branch "gpipe_compat"
 *   4.0.12  01/07/2025          tblastn: -db --> -subject
@@ -348,7 +349,7 @@ using namespace GFF_sp;
 const string dataVer_min ("2024-08-14.2");
   // 3.12: "2023-12-15.2"
   // 3.11: "2021-02-18.1"  
-const string stxTyperVersion ("1.0.30");  
+const string stxTyperVersion ("1.0.31");  
 
 
 
